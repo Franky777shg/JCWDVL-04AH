bace0779d579c417983ce65f866e0a30f1a67af6
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2021-12-08 18:56:21 +0700

    variabel dan tipe data

---
+++ AStalker.js
@@ -0,0 +1,5 @@
+let word = "JavAScript MantAp"
+
+let result = word.replace(/a/g, "a.").result.replace(/A/g, 'A.')
+
+console.log(result)
---
+++ MultiplyFive.js
@@ -0,0 +1,9 @@
+let start = -5
+
+// ** => operasi pangkat
+let result = start * (5 ** Math.abs(start).toString().length)
+// 10.toString() = "10"
+// "10".length = 2
+// let result = 10 * 5^2
+
+console.log(result)
